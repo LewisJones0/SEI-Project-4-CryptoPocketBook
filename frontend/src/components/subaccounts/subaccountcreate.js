1bf949be745860e5b4b80ffee61517e8e0ccaf3c
Lewis Jones <lewisjones0000@gmail.com>
2020-10-26 22:28:20 +0000

    Working Create sub

--- frontend/src/components/subaccounts/subaccountcreate.js
+++ frontend/src/components/subaccounts/subaccountcreate.js
@@ -6,20 +6,10 @@ import { newSub } from '../../lib/api'
 class SubaccountCreate extends React.Component {
   state = {
     formData: {
-      name: null
-    },
-    profile: {},
-    subaccounts: {}
+      name: ''
+    }
   }
 
-  async componentDidMount() {
-    const getData = await getUser()
-    console.log(getData)
-    this.setState({
-      profile: getData.data,
-      subaccounts: getData.data.subaccounts
-    })
-  }
 
   handleChange = event => {
     const formData = {
@@ -54,23 +44,24 @@ class SubaccountCreate extends React.Component {
             <Card className="CreateSubAccount">
               <Form.Group as={Row} controlId="formBasicname">
                 <Col sm={12}>
-                  <Form.Label column sm={12} className='SAFormTitle'>
-                      Desired SubAccount Name
-                  </Form.Label>
-                  <Form.Control 
-                    className='SAFormControl'
-                    type="name" 
-                    placeholder="Input here"
-                    name="name"
-                    value={name}
-                    onChange={this.handleChange} />
+                  <Form onSubmit={this.handleSubmit}>
+                    <Form.Label column sm={12} className='SAFormTitle'>
+                        Desired SubAccount Name
+                    </Form.Label>
+                    <Form.Control 
+                      className='SAFormControl'
+                      type="name" 
+                      placeholder="Input here"
+                      name="name"
+                      value={name}
+                      onChange={this.handleChange}/>
+                    <Button variant="danger" type="submit" block>Create Subaccount</Button>
+                  </Form>
                 </Col>
               </Form.Group>
-              <Button variant="danger" type="submit" block>Create Subaccount</Button>
             </Card>
           </Col>
         </Row>
-        {/* </Row> */}
       </>
     )
   }
